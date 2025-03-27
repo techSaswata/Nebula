62f02adf238851105d134ee96e2384151d0de072
techSaswata <saswata.24bcs10248@sst.scaler.com>
2025-03-28 00:52:25 +0530

    fixed Error in google signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import { redirect } from 'next/navigation'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
-  const supabase = createServerComponentClient({ cookies })
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
+  
   const { data: { session } } = await supabase.auth.getSession()
 
   if (session) {
